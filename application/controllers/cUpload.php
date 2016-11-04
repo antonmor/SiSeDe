2d47399e344dc7e15c9ee8db2536877b2ff32082
ANTONIO MORENO JAUREGUI <ingantonmor@gmail.com>
2016-11-04 17:11:56 -0600

    123

--- application/controllers/cUpload.php
+++ application/controllers/cUpload.php
@@ -19,12 +19,20 @@
 		public function do_upload()
 		{
 			$this->load->model('mLogin'); //llamamos al modelo
+			
+			//tabla Expediente debe llevar:
+		    $jsonTExpediente  = array('idCreaEx' =>  $this->input->post('status'));
+
 			//$idExp =           //id Expediente control de la tabla
 			$idCreaExp=$_SESSION["Persona_id"]; //idPersona sesion que crea la demanda
 		    $idPDemandante=$this->input->post('idPDemandante'); //demandante
 			$idPDemandado=$this->input->post('idPDemandado'); //demandado
-			echo $fecha=$this->input->post('fecha'); // fecha en que se crea.
+			$fecha=$this->input->post('fecha'); // fecha en que se crea.
+			//Expediente
+			$Descripcion=$this->input->post('Des'); //Descripci[on]
+			$est=$this->input->post('status');
 			$tipo=$this->input->post('tipo'); //tipo del documento que se va a subir (Demanda)
+			echo json_encode($jsonTExpediente);
 			//$idPpresenta
 
 			/*
